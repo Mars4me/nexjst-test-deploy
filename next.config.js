aca263351014dc34f9b023ae02794b89bafdec74
Mars4me <lmarsl1994@gmail.com>
2022-09-06 12:07:43 +0300

    add basepath for assets

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader:'akamai',
-    path: ''
+    loader: "akamai",
+    path: "",
   },
+  basePath: "/nexjst-test-deploy",
+  assetPrefix: "nexjst-test-deploy",
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
